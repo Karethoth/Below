4f3250d16410c2fed6cf0686ed05235629d3cdb8
Arttu Multanen <karethoth@gmail.com>
2014-06-01 07:28:01 +0300

    Added worldObjectType enumeration.

---
+++ src/world/worldObjectTypes.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#define WORLD_OBJECT_TYPE_LENGTH sizeof( unsigned char );
+
+
+enum WorldObjectType : unsigned char
+{
+	UNDEF_OBJECT_TYPE = 0,
+	WORLD_NODE_OBJECT_TYPE,
+	ENTITY_OBJECT_TYPE
+};
+
